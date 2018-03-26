86316ae1cbfa95efd0ea70139d40ede06a1951a0
Logan Hendricks <llhendricks@xactware.com>
2018-03-26 17:41:57 -0600

    confirmed session error issue. potentially check and force failure in after block, and see if that causes a session crash?

--- protractor/step_definitions/util.js
+++ protractor/step_definitions/util.js
@@ -17,6 +17,8 @@ module.exports.takeScreenShot = function(parameters){
 						stream.write(new Buffer(png, "base64"));
 						stream.end();
 						resolve();
+					}).catch(function() {
+						reject();
 					});
 				};
 				if(exists === false) {
@@ -34,28 +36,28 @@ module.exports.takeScreenShot = function(parameters){
 	});
 };
 
-// function getDateAsString(parameters) {
-// 	let date = new Date();
-// 	let result = "" + date.getFullYear();
-// 	result = result + ".";
-// 	result = result + padZeros({ startNumber: date.getMonth(), totalDigits: 2 });
-// 	result = result + ".";
-// 	result = result + padZeros({ startNumber: date.getDate(), totalDigits: 2 });
-// 	result = result + "_";
-// 	result = result + padZeros({ startNumber: date.getHours(), totalDigits: 2 });
-// 	result = result + ".";
-// 	result = result + padZeros({ startNumber: date.getMinutes(), totalDigits: 2 });
-// 	result = result + ".";
-// 	result = result + padZeros({ startNumber: date.getSeconds(), totalDigits: 2 });
-// 	result = result + "_";
-// 	result = result + padZeros({ startNumber: date.getMilliseconds(), totalDigits: 3 });
-// 	return result;
-// }
-//
-// function padZeros(parameters) {
-// 	let result = parameters.startNumber.toString();
-// 	while(result.length < parameters.totalDigits) {
-// 		result = "0" + result;
-// 	}
-// 	return result;
-// }
+function getDateAsString(parameters) {
+	let date = new Date();
+	let result = "" + date.getFullYear();
+	result = result + ".";
+	result = result + padZeros({ startNumber: date.getMonth(), totalDigits: 2 });
+	result = result + ".";
+	result = result + padZeros({ startNumber: date.getDate(), totalDigits: 2 });
+	result = result + "_";
+	result = result + padZeros({ startNumber: date.getHours(), totalDigits: 2 });
+	result = result + ".";
+	result = result + padZeros({ startNumber: date.getMinutes(), totalDigits: 2 });
+	result = result + ".";
+	result = result + padZeros({ startNumber: date.getSeconds(), totalDigits: 2 });
+	result = result + "_";
+	result = result + padZeros({ startNumber: date.getMilliseconds(), totalDigits: 3 });
+	return result;
+}
+
+function padZeros(parameters) {
+	let result = parameters.startNumber.toString();
+	while(result.length < parameters.totalDigits) {
+		result = "0" + result;
+	}
+	return result;
+}
