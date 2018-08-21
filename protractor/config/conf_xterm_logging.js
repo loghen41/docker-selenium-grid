2190ebc8ff1263d625483b7227dac347fc06f696
Logan Hendricks <llhendricks@xactware.com>
2018-08-21 15:37:07 -0600

    xterm logging conf

---
+++ protractor/config/conf_xterm_logging.js
@@ -0,0 +1,24 @@
+var path = process.cwd();
+
+exports.config = {
+	seleniumAddress: 'http://localhost:4444/wd/hub',
+
+	// set to "custom" instead of cucumber.
+	framework: 'custom',
+
+	// path relative to the current config file
+	frameworkPath: require.resolve('protractor-cucumber-framework'),
+
+	// require feature files
+	specs: [ path + '/features/10featuretest/*.feature' ],
+
+	capabilities: {
+		'browserName': 'chrome',
+		'loggingPrefs': {'driver': 'INFO', 'browser': 'INFO'},
+	},
+
+	cucumberOpts: {
+		format: ["json:results.json", `../node_modules/cucumber-pretty`],
+		require: [ path + '/step_definitions/**/*.js' ]
+	}
+};
